c5b1ec3636418bcecb203c049739378cd72ebf74
Sarahwaleed20 <sh2400281@tkh.edu.eg>
2025-12-12 22:10:14 +0200

    backend edit 15

--- routes/expenseroutes.js
+++ routes/expenseroutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../controllers/authcontroller');
 const { addExpense, getExpenses } = require('../controllers/expensecontroller');
-const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/add', verifyToken, addExpense);
 router.get('/', verifyToken, getExpenses);
